8ce99cc4e837273820e623f79f8efdf4d3d3c6b7
guillermo <guillermoliceotecnico@email.com>
2025-05-02 02:10:09 -0400

    guilleee

--- hola.py
+++ hola.py
@@ -71,16 +71,69 @@
 #for i in range(1,numero+1,2):
 #    print(i)
 
-productos={}
-total_precio=0
-cantidad_productos=0
+#productos={}
+#total_precio=0
+#cantidad_productos=0
 
-def agregar_producto():
-    global total_recio ,cantidad_productos
-nombre_producto=input("ingrese el nombre del producto:")
-precio_producto=float(input("ingrese el precio del producto:"))
-productos[nombre_producto]=precio_producto
-total_precio+=precio_producto
-cantidad_productos+=1
-print(f"producto agregado:{nombre_producto}-precio: ${precio_producto:.2f}")
+#def agregar_producto():
+    #global total_recio ,cantidad_productos
+#nombre_producto=input("ingrese el nombre del producto:")
+#precio_producto=float(input("ingrese el precio del producto:"))
+#productos[nombre_producto]=precio_producto
+#total_precio+=precio_producto
+#cantidad_productos+=1
+#print(f"producto agregado:{nombre_producto}-precio: ${precio_producto:.2f}")
 
+
+
+
+
+#clave correcta predevinida
+
+#clave_correcta="guille_ulloa_vera:"
+
+
+#while True:
+
+  #clave_ingresada=input("ingrese clave:")
+  #if clave_ingresada==clave_correcta:
+     #print("clave correcta!, welcome: ")
+     #break
+  #print("Error. Ingrese clave correcta.")
+
+# algoritmo liviano_pesado_citycar
+
+# iniciamos la variable total
+#total=0
+
+# solicitamos el numero de vehiculos
+
+#num=int(input("ingrese el numero de vehiculos:"))
+
+
+#iteramos desde 1 hasta num
+#for i in range(1,num+1):
+    #print(f"\nvehiculo #{i}")
+    #print("seleccione el tipo de vehiculo:")
+    #print("1: auto liviano")
+    #print("2: auto pesado")
+    #print("3: citycar")
+
+    #auto=int(input("opcion (1-3):"))
+
+    #if auto == 2:
+        #print("auto pesado, costo 5000$")
+        #total+=5000
+    #elif auto == 1:
+        #print("auto liviano, costo 3500$")
+        #total+=3500
+    #elif auto == 3:
+        #print("citycar, costo 1500$")
+        #total+=1500
+    #else:
+        #print("error, seleccione una opcion valida")
+
+        #mostrar resultados
+
+#print("\nlas ganancias diarias son $", total)
+#print("las ganancias diarias con iva (19%) son $", round(total*1.19, 2))
